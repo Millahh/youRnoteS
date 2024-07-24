c4e0eb2518167e33c56f8f937f1b852e4383f655
millahh <millahmillah37@gmail.com>
2024-07-25 02:46:02 +0700

    updated

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,9 @@ use App\Http\Controllers\NoteController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', function () {
-    return view('/dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/', function () {
+//     return view('/dashboard');
+// })->middleware(['auth'])->name('dashboard');
 // FORM LARAVEL BREEZE
 
 // Route::get('/', function () {
@@ -19,10 +18,10 @@ Route::get('/', function () {
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
 // MODIFY ROUTES
-Route::middleware(["auth","verified"])->group(function () {
+Route::middleware(["auth"])->group(function () {
     Route::resource('note', NoteController::class);
     // SHORTHAND FORM OF THESE ROUTES BELOW
-    // Route::get('/note', [NoteController::class,'index'])->name('welcome');
+    Route::get('/', [NoteController::class,'index'])->name('dashboard');
     // Route::get('/note/create', [NoteController::class, 'create'])->name('note.index');
     // Route::post('/note', [NoteController::class, 'store'])->name('note.store');
     // Route::get('/note/{id}', [NoteController::class,'show'])->name('note.show');
